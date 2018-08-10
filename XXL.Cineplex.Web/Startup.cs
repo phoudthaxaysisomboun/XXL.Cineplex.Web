29621b5f739892a3867ea9198a65762580641819
Phoudthaxay Sisomboun <phoudthaxaysisomboun@outlook.com>
2018-08-10 18:29:14 +0700

    Add project files.

---
+++ XXL.Cineplex.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XXL.Cineplex.Web.Startup))]
+namespace XXL.Cineplex.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
